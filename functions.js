7892fba6e4cfa1f0cdede0a3df8bb6e5a9a0e0f9
Csillag61 <csillagok@gmail.com>
2019-05-02 11:28:19 -0400

    closures, recursion

--- functions.js
+++ functions.js
@@ -63,3 +63,19 @@ const hummus = function(factor) {
 
  //recursion
  //A function that calls itself is called recursive. 
+ function findSolution(target) {
+    function find(current, history) {//inner function- find-makes the recursion
+      if (current == target) {
+        return history;//returns how we find the target
+      } else if (current > target) {
+        return null;//if no solution can be found, returns null
+      } else {
+        return find(current + 5, `(${history} + 5)`) ||
+               find(current * 3, `(${history} * 3)`);
+      }
+    }
+    return find(1, "1");
+  }
+  
+  console.log(findSolution(24));
+  // → (((1 * 3) + 5) * 3)
